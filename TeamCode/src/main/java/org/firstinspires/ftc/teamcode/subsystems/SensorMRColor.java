5ad62270a845ab000273dc960e16fa8d97fbf1ea
Karl W. Scherer <47581330+karlscherer@users.noreply.github.com>
2025-11-27 20:41:14 -0600

    changed mechanisms to subsystems

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SensorMRColor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SensorMRColor.java
@@ -27,7 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.teamcode.mechanisms;
+package org.firstinspires.ftc.teamcode.subsystems;
 
 import android.app.Activity;
 import android.graphics.Color;
